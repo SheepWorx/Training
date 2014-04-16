5793b29e7535186de9c3248e71c2647e34bfe928
SheepWorx <deonpmeyer@gmail.com>
2014-04-16 20:13:55 +0200

    final final changes, tee hee

--- OpenClosedPrinciple/OpenClosedPrinciple/3. OCP by Extension/After/Product.cs
+++ OpenClosedPrinciple/OpenClosedPrinciple/3. OCP by Extension/After/Product.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace OpenClosedPrinciple._3._OCP_by_Extension.After
 {
-    /// <summary>
-    /// the only change we needed to make here was to make the GetProductDescription method virtual
-    /// 
-    /// thus, we can inherit of this class and override it
-    /// </summary>
     public class Product
     {
         public string Name { get; set; }
